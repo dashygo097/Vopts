7a37a7cb657ea88c5542273033090ed4af5ffd8a
dashygo097 <dashygo097:xyz334455678@github.com>
2025-04-12 14:03:59 +0800

    small update

--- app/uart.scala
+++ app/uart.scala
@@ -1,13 +1,6 @@
 package app
 
-import global._
 import uart.UARTCore
-import java.io.{BufferedWriter, File, FileWriter}
-
-import chisel3._
-import chisel3.util._
-
-import _root_.circt.stage.ChiselStage
 
 object UART extends App {
   VerilogEmitter.parse(new UARTCore(115200), "uart.sv")
