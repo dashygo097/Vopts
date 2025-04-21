5dbc417a0338a10fad53f7cf23e9cbf0f419fa6d
dashygo097 <dashygo097@github.com>
2025-04-21 23:53:43 +0800

    fix some bugs

--- autotest.sh
+++ autotest.sh
@@ -6,14 +6,19 @@ BUILD_DIR="$BASE_DIR/build"
 run_test() {
 	cd "$BUILD_DIR" || exit
 	printf "\e[1;31m[NOTE] Choose the testbench.\n\e[0m"
+
 	tb_file=$(find . -type f -name "*_tb.sv" -o -name "*_tb.v" | fzf)
 	module_file=$(basename "$tb_file" | sed 's/_tb\.sv//')
-	verilator --cc --exe --build --binary --trace "$tb_file" -o "V$(basename "$module_file" .sv)"
+	ext=".${module_file##*.}"
+
+	verilator --cc --exe --build --binary --trace "$tb_file" -o "V$(basename "$module_file" "$ext")"
+
 	cd "./obj_dir" || exit
-	"./V$(basename "$module_file" .sv)"
+	"./V$(basename "$module_file" "$ext")"
+
 	# run if .vcd file exists
-	if [ -f "$(basename "$module_file" .sv).vcd" ]; then
-		gtkwave "$(basename "$module_file" .sv).vcd"
+	if [ -f "$(basename "$module_file" "$ext").vcd" ]; then
+		gtkwave "$(basename "$module_file" "$ext").vcd"
 	fi
 
 	cd "$BASE_DIR" || exit
