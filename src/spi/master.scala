990f20efd9bbe46cb958c94e0bc0818a8ef12395
dashygo-air <dashygo097@gmail.com>
2025-04-26 01:21:02 +0800

    starts spi

---
+++ src/spi/master.scala
@@ -0,0 +1,21 @@
+package spi
+
+import utils.Config
+
+import chisel3._
+import chisel3.util._
+
+
+class SPIMasterInterfaceIO extends Bundle {
+  val sck = Output(Bool())  
+  val mosi = Output(Bool()) 
+  val miso = Input(Bool())  
+  val ss = Output(Bool())
+}
+
+class SPIMasterDataIO(sDataWidth: Int) extends Bundle {
+  val txData = Input(UInt(sDataWidth.W)) 
+  val rxData = Output(UInt(sDataWidth.W))
+  val start = Input(Bool()) 
+  val done = Output(Bool())
+}
