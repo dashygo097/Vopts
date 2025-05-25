7c64098184858f3316ecce3011c0664051208cd0
dashygo097 <dashygo097@github.com>
2025-05-26 00:55:06 +0800

    add:some apps for guang

---
+++ app/guangsai.scala
@@ -0,0 +1,12 @@
+package app
+
+import peripheral.{ADC122S625Core, MCP4921Core}
+import app.VerilogEmitter
+
+object ADC122S625 extends App {
+  VerilogEmitter.parse(new ADC122S625Core, "adc122s625.sv")
+}
+
+object MCP4921 extends App {
+  VerilogEmitter.parse(new MCP4921Core, "mcp4921.sv")
+}
--- src/utils/io.scala
+++ src/utils/io.scala
@@ -15,9 +15,19 @@ class SISO[T <: Data](gen: T) extends Bundle {
   val out = Output(gen)
 }
 
+class DSISO[InT <: Data, OutT <: Data](inGen: InT, outGen: OutT) extends Bundle {
+  val in = Input(inGen)
+  val out = Output(outGen)
+}
+
 class SIMO[T <: Data](gen: T, outs: Int) extends Bundle {
   val in = Input(gen)
   val out = Output(Vec(outs, gen))
+}
+
+class DSIMO[InT <: Data, OutT <: Data](inGen: InT, outs: (OutT, Int)) extends Bundle {
+  val in = Input(inGen)
+  val out = Output(Vec(outs._2, outs._1))
 }
 
 class MISO[T <: Data](gen: T, ins: Int) extends Bundle {
@@ -25,8 +35,18 @@ class MISO[T <: Data](gen: T, ins: Int) extends Bundle {
   val out = Output(gen)
 }
 
+class DMISO[InT <: Data, OutT <: Data](ins: (InT, Int), outGen: OutT) extends Bundle {
+  val in = Input(Vec(ins._2, ins._1))
+  val out = Output(outGen)
+}
+
 class MIMO[T <: Data](gen: T, ins: Int, outs: Int) extends Bundle {
   val in = Input(Vec(ins, gen))
   val out = Output(Vec(outs, gen))
+}
+
+class DMIMO[InT <: Data, OutT <: Data](ins: (InT, Int), outs: (OutT, Int)) extends Bundle {
+  val in = Input(Vec(ins._2, ins._1))
+  val out = Output(Vec(outs._2, outs._1))
 }
 
