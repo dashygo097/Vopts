6d733ea9282b95e887d6bf51240724c57e36e664
dashygo097 <dashygo097@github.com>
2025-05-27 17:25:16 +0800

    optimization: a more professional and readable code style; optimization: support general type for some modules

--- src/func/resample/decimator.scala
+++ src/func/resample/decimator.scala
@@ -1,18 +1,17 @@
 package func.resample
 
-import utils._
 import chisel3._
 import chisel3.util._
 
-class DecimatorIO extends Bundle {
-  val in = Input(new FP)
-  val out = Output(new FP)
+class DecimatorIO[T <: Data](gen: T) extends Bundle {
+  val in = Input(gen)
+  val out = Output(gen)
   val in_valid = Input(Bool())
   val out_valid = Output(Bool())
 }
 
-class DecimatorCore(factor: Int) extends Module {
-  val io = IO(new DecimatorIO)
+class DecimatorCore[T <: Data](gen: T, factor: Int) extends Module {
+  val io = IO(new DecimatorIO(gen))
   val count = RegInit(0.U(log2Ceil(factor).W))
 
   count := Mux(io.in_valid, Mux(count === (factor - 1).U, 0.U, count + 1.U), count)
@@ -25,8 +24,8 @@ object Decimator {
   
   var _factor: Int = 1
 
-  def apply(in: FP, in_valid: Bool): (FP, Bool) = {
-    val decimator = Module(new DecimatorCore(_factor))
+  def apply[T <: Data](in: T, in_valid: Bool): (T, Bool) = {
+    val decimator = Module(new DecimatorCore(in, _factor))
     decimator.io.in := in
     decimator.io.in_valid := in_valid
     (decimator.io.out, decimator.io.out_valid)
