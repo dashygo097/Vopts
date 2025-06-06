b6f6dccb13db33e416fa66d4acd4fa44dd1f94fd
dashygo097 <dashygo097@github.com>
2025-06-06 16:48:44 +0800

    debug

--- app/guangsai/top.scala
+++ app/guangsai/top.scala
@@ -7,43 +7,38 @@ import app.VerilogEmitter
 import utils._
 import chisel3._
 
-class ADC1IO extends Bundle {
-  val ADC1_EN = Input(Bool())
-  val ADC1_CS_N = Output(Bool())
-  val ADC1_SDO = Input(Bool())
-  val ADC1_DATA_A = Output(new FP(12, 11))
-  val ADC1_DATA_B = Output(new FP(12, 11))
+class ADCIO extends Bundle {
+  val CS = Input(Bool())
+  val DOUT = Input(Bool())
+  val SCLK = Input(Clock())
 
-  val ADC1_SCLK = Input(Clock())
-  val ADC1_GATE_IN = Input(Bool())
-  val ADC1_GATE_OUT = Input(Bool())
-  
-  val ADC1_FULL_A = Output(Bool())
-  val ADC1_FULL_B = Output(Bool())
-  val ADC1_EMPTY_A = Output(Bool())
-  val ADC1_EMPTY_B = Output(Bool())
+  val DATA_A = Output(new FP(12, 11))
+  val DATA_B = Output(new FP(12, 11))
+
+  val FULL_A = Output(Bool())
+  val FULL_B = Output(Bool())
+  val EMPTY_A = Output(Bool())
+  val EMPTY_B = Output(Bool())
+
 }
 
 class Top extends Module {
-  val adc1_io = IO(new ADC1IO)
+  val DFB1_ADC = IO(new ADCIO)
   val adc1 = Module(new ADC122S625Core(128))
 
-  // ADC1 INPUT
-  adc1.io.en := adc1_io.ADC1_EN
-  adc1.io.sdo := adc1_io.ADC1_SDO
-  adc1.io.sclk := adc1_io.ADC1_SCLK
-  adc1.io.gateIn := adc1_io.ADC1_GATE_IN
-  adc1.io.gateOut := adc1_io.ADC1_GATE_OUT
+  adc1.io.cs_n := DFB1_ADC.CS
+  adc1.io.sdo := DFB1_ADC.DOUT
+  adc1.io.sclk := DFB1_ADC.SCLK
+  adc1.io.gateIn := 1.B
+  adc1.io.gateOut := 1.B
 
-  // ADC1 OUTPUT
-  adc1_io.ADC1_CS_N := adc1.io.cs_n
-  adc1_io.ADC1_DATA_A := adc1.io.dataA
-  adc1_io.ADC1_DATA_B := adc1.io.dataB
-  adc1_io.ADC1_FULL_A := adc1.io.fullA
-  adc1_io.ADC1_FULL_B := adc1.io.fullB
-  adc1_io.ADC1_EMPTY_A := adc1.io.emptyA
-  adc1_io.ADC1_EMPTY_B := adc1.io.emptyB
+  DFB1_ADC.DATA_A := adc1.io.dataA
+  DFB1_ADC.DATA_B := adc1.io.dataB
 
+  DFB1_ADC.FULL_A := adc1.io.fullA
+  DFB1_ADC.FULL_B := adc1.io.fullB
+  DFB1_ADC.EMPTY_A := adc1.io.emptyA
+  DFB1_ADC.EMPTY_B := adc1.io.emptyB
 }
 
 object Top extends App {
