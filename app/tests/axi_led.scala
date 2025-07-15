79626cb8b5214236991f306b82fe59cc0caff9de
dashygo097 <dashygo097@gmail.com>
2025-07-15 22:42:25 +0800

    update: axi_led_test

--- app/tests/axi_led.scala
+++ app/tests/axi_led.scala
@@ -15,34 +15,45 @@ class AXILedSlave(addrWidth: Int, dataWidth: Int) extends Module {
 
   io.axi.writeAddr.ready := true.B
   io.axi.writeData.ready := true.B
-  io.axi.writeResp.valid := false.B
-  io.axi.writeResp.bits := 0.U
 
-  val write_fire = io.axi.writeAddr.valid && io.axi.writeData.valid
-  when(write_fire) {
+  val writeRespValid = RegInit(false.B)
+
+  when(io.axi.writeAddr.fire && io.axi.writeData.fire && !writeRespValid) {
     when(io.axi.writeAddr.bits.addr === 0.U) {
       ledReg := io.axi.writeData.bits.data
     }
-    io.axi.writeResp.valid := true.B
-    io.axi.writeResp.bits := 0.U
+    writeRespValid := true.B
   }
 
-  io.axi.readAddr.ready := true.B
+  io.axi.writeResp.valid := writeRespValid
+  io.axi.writeResp.bits := 0.U
+  when(writeRespValid && io.axi.writeResp.ready) {
+    writeRespValid := false.B
+  }
 
-  io.axi.readData.valid := io.axi.readAddr.valid
+  val readAddrReg = Reg(UInt(addrWidth.W))
+  val readPending = RegInit(false.B)
+
+  when(io.axi.readAddr.fire && !readPending) {
+    readAddrReg := io.axi.readAddr.bits.addr
+    readPending := true.B
+  }
+
+  io.axi.readAddr.ready := !readPending
+  io.axi.readData.valid := readPending
   io.axi.readData.bits.resp := 0.U
-  io.axi.readData.bits.data := Mux(
-    io.axi.readAddr.bits.addr === 0.U,
-    ledReg,
-    0.U
-  )
+  io.axi.readData.bits.data := Mux(readAddrReg === 0.U, ledReg, 0.U)
+
+  when(io.axi.readData.fire) {
+    readPending := false.B
+  }
 }
 
 class Top extends Module {
   val led = IO(Output(Bool()))
-  val axi = IO(new AXILiteExternalIF(32, 4)).suggestName("S_AXI")
+  val axi = IO(new AXILiteExternalIF(32, 8)).suggestName("S_AXI")
 
-  val slave = Module(new AXILedSlave(32, 4))
+  val slave = Module(new AXILedSlave(32, 8))
 
   axi.connect(slave.io.axi)
   led := slave.io.led
