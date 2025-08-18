56ac0059566d4cfd781832cd42d6936db36aa7c5
dashygo097 <dashygo097@gmail.com>
2025-08-18 20:11:34 +0800

    update: dual port ram; optim: ram

--- src/mem/ram/async.scala
+++ src/mem/ram/async.scala
@@ -1,37 +1,31 @@
-package mem.sram
+package mem.ram
 
 import chisel3._
 import chisel3.util._
-
-class AsyncRWIO extends Bundle {
-  val we = Input(Bool())
-  val re = Input(Bool())
-
-  val rd_clk = Input(Clock())
-  val wr_clk = Input(Clock())
-}
 
 class AsyncRAMIO[T <: Data](gen: T, size: Int) extends Bundle {
   val addr = Input(UInt(log2Ceil(size).W))
   val dataIn = Input(gen)
   val dataOut = Output(gen)
 
-  val en = new AsyncRWIO
+  val we = Input(Bool())
+  val re = Input(Bool())
+  val wrClk = Input(Clock())
+  val rdClk = Input(Clock())
 }
 
 class AsyncRAMCore[T <: Data](gen: T, size: Int) extends Module {
-  val io = IO(new AsyncRAMIO(gen, size)).suggestName("A_RAM")
+  val io = IO(new AsyncRAMIO(gen, size)).suggestName("ASYNC_RAM")
 
   val mem = SyncReadMem(size, gen)
   val rdata = Reg(gen)
   io.dataOut := DontCare
 
-  when(io.en.we) {
-    mem.write(io.addr, io.dataIn, io.en.wr_clk)
+  when(io.we) {
+    mem.write(io.addr, io.dataIn, io.wrClk)
   }
-
-  when (io.en.re) {
-    io.dataOut := mem.read(io.addr, io.en.rd_clk)
+  when (io.re) {
+    io.dataOut := mem.read(io.addr, io.rdClk)
   } 
 }
  
