70ab0d7f7bb4ff7a0934efecabc2b90d99fb2e6a
dashygo097 <dashygo097@gmail.com>
2025-09-14 17:29:13 +0800

    update

--- src/mem/register/axi_mmap.scala
+++ src/mem/register/axi_mmap.scala
@@ -19,6 +19,7 @@ class AXISlaveMMap(dataWidth: Int, addrWidth: Int, mmap: Seq[RegisterFactory]) e
 
   val ext_axi = IO(new AXILiteExternalIO(dataWidth, addrWidth)).suggestName("S_AXI")
   val axi = Wire(new AXILiteSlaveIO(dataWidth, addrWidth))
+
   val addr_lsb = log2Ceil(dataWidth / 8)
   val opt_mem_addr_bits = log2Ceil(mmap.length)
 
