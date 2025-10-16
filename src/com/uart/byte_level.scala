c2fe0e85aa0d2a268daa44ee75fb950a74676dd6
dashygo097 <dashygo097@gmail.com>
2025-10-17 00:25:59 +0800

    problem with uart cmd

--- src/com/uart/byte_level.scala
+++ src/com/uart/byte_level.scala
@@ -1,7 +1,10 @@
 package com.uart
-
 import chisel3._
 import chisel3.util._
+
+object UartState extends ChiselEnum {
+  val IDLE, START, DATA, STOP = Value
+}
 
 class ByteLevelUartTXIO extends Bundle {
   val txd     = Output(Bool())
@@ -16,40 +19,42 @@ class ByteLevelUartRXIO extends Bundle {
 }
 
 class ByteLevelUartTX(baudRate: Int, clkFreq: Int) extends Module {
+  override def desiredName: String = s"uart_tx_byte_level_b${baudRate}_f${clkFreq}"
   val io              = IO(new ByteLevelUartTXIO).suggestName("UART_TX")
   val baudTickDivider = clkFreq / baudRate
-
+  
   val state = RegInit(UartState.IDLE)
-
   val data        = Reg(UInt(8.W))
-  val tick        = RegInit(false.B)
   val bitCounter  = RegInit(0.U(3.W))
   val tickCounter = RegInit(0.U(log2Ceil(baudTickDivider).W))
-
-  tickCounter      := Mux(tickCounter === (baudTickDivider - 1).U, 0.U, tickCounter + 1.U)
-  tick             := Mux(tickCounter === (baudTickDivider - 1).U, true.B, false.B)
+  
+  // Tick generation
+  val tick = tickCounter === (baudTickDivider - 1).U
+  tickCounter := Mux(tick, 0.U, tickCounter + 1.U)
+  
+  // Default outputs
   io.busy          := state =/= UartState.IDLE
-  io.txd           := true.B
-  io.channel.ready := state === UartState.IDLE
-
+  io.txd           := true.B  // Default idle high
+  io.channel.ready := false.B
+  
   switch(state) {
     is(UartState.IDLE) {
+      io.channel.ready := true.B
       when(io.channel.valid) {
         state := UartState.START
         data  := io.channel.bits
+        tickCounter := 0.U
       }
     }
-
     is(UartState.START) {
-      io.txd := false.B
+      io.txd := false.B  // Start bit is LOW
       when(tick) {
         state      := UartState.DATA
         bitCounter := 0.U
       }
     }
-
     is(UartState.DATA) {
-      io.txd := data(0)
+      io.txd := data(0)  // Send LSB first
       when(tick) {
         when(bitCounter === 7.U) {
           state := UartState.STOP
@@ -59,9 +64,8 @@ class ByteLevelUartTX(baudRate: Int, clkFreq: Int) extends Module {
         }
       }
     }
-
     is(UartState.STOP) {
-      io.txd := true.B
+      io.txd := true.B  // Stop bit is HIGH
       when(tick) {
         state := UartState.IDLE
       }
@@ -70,41 +74,47 @@ class ByteLevelUartTX(baudRate: Int, clkFreq: Int) extends Module {
 }
 
 class ByteLevelUartRX(baudRate: Int, clkFreq: Int) extends Module {
+  override def desiredName: String = s"uart_rx_byte_level_b${baudRate}_f${clkFreq}"
   val io              = IO(new ByteLevelUartRXIO).suggestName("UART_RX")
   val baudTickDivider = clkFreq / baudRate
-
+  
   val state = RegInit(UartState.IDLE)
-
   val data        = Reg(UInt(8.W))
-  val tick        = RegInit(false.B)
   val bitCounter  = RegInit(0.U(3.W))
   val tickCounter = RegInit(0.U(log2Ceil(baudTickDivider).W))
-
-  tickCounter      := Mux(tickCounter === (baudTickDivider - 1).U, 0.U, tickCounter + 1.U)
-  tick             := Mux(tickCounter === (baudTickDivider - 1).U, true.B, false.B)
-  io.channel.valid := false.B
-  io.channel.bits  := data
-  io.error         := false.B
-
+  
+  // Tick generation
+  val tick = tickCounter === (baudTickDivider - 1).U
+  tickCounter := Mux(tick, 0.U, tickCounter + 1.U)
+  
+  // Double-register rxd to avoid metastability
   val rxd = RegNext(RegNext(io.rxd))
-
+  
+  // Default outputs
+  io.channel.valid := false.B
+  io.channel.bits  := 0.U
+  io.error         := false.B
+  
   switch(state) {
     is(UartState.IDLE) {
-      when(!rxd) {
+      when(!rxd) {  // Detect start bit (HIGH to LOW transition)
         state := UartState.START
+        tickCounter := (baudTickDivider / 2).U
       }
     }
-
     is(UartState.START) {
       when(tick) {
-        state      := UartState.DATA
-        bitCounter := 0.U
+        when(!rxd) {  // Verify start bit is still LOW
+          state      := UartState.DATA
+          bitCounter := 0.U
+        }.otherwise {
+          state := UartState.IDLE
+        }
       }
     }
-
     is(UartState.DATA) {
       when(tick) {
-        data := Cat(rxd, data(7, 1))
+        data := Cat(rxd, data(7, 1))  // Shift in from MSB, receive LSB first
         when(bitCounter === 7.U) {
           state := UartState.STOP
         }.otherwise {
@@ -112,11 +122,11 @@ class ByteLevelUartRX(baudRate: Int, clkFreq: Int) extends Module {
         }
       }
     }
-
     is(UartState.STOP) {
       when(tick) {
         io.channel.valid := true.B
-        io.error         := !rxd
+        io.channel.bits  := data
+        io.error         := !rxd  // Error if stop bit is not HIGH
         state            := UartState.IDLE
       }
     }
@@ -124,14 +134,15 @@ class ByteLevelUartRX(baudRate: Int, clkFreq: Int) extends Module {
 }
 
 class ByteLevelUart(baudRate: Int, clkFreq: Int) extends Module {
+  override def desiredName: String = s"uart_byte_level_b${baudRate}_f${clkFreq}"
   val io = IO(new Bundle {
     val tx = new ByteLevelUartTXIO
     val rx = new ByteLevelUartRXIO
   }).suggestName("UART")
-
+  
   val tx = Module(new ByteLevelUartTX(baudRate, clkFreq))
   val rx = Module(new ByteLevelUartRX(baudRate, clkFreq))
-
+  
   tx.io <> io.tx
   rx.io <> io.rx
 }
