8dc5973ebcd56f9bf74361e4c4e1f617d7354172
dashygo097 <dashygo097@github.com>
2025-10-27 00:31:37 +0800

    fix: fpconverter

--- src/utils/datatype/fp/converter.scala
+++ src/utils/datatype/fp/converter.scala
@@ -1,7 +1,5 @@
 package utils
 
-import scala.math.pow
-
 import chisel3._
 
 class FPConverterIO(in_dw: Int, in_bp: Int, out_dw: Int, out_bp: Int) extends Bundle {
@@ -14,12 +12,16 @@ class FPConverter(in_dw: Int, in_bp: Int, out_dw: Int, out_bp: Int) extends Modu
   val io                   = IO(new FPConverterIO(in_dw, in_bp, out_dw, out_bp))
   val shiftAmt             = out_bp - in_bp
   val shifted              = if (shiftAmt > 0) {
-    io.in * pow(2, shiftAmt)
+    io.in.value << shiftAmt.U
   } else if (shiftAmt < 0) {
-    io.in * (1 / pow(2, -shiftAmt))
+    io.in.value >> (-shiftAmt).U
   } else {
-    io.in
+    io.in.value
   }
 
-  io.out := shifted.value.asSInt.pad(in_dw).asSInt.asTypeOf(new FP(out_dw, out_bp))
+  io.out := new FP(out_dw, out_bp).fromSInt(shifted)
+}
+
+object FPConverter extends App {
+  VerilogEmitter.parse(new FPConverter(32, 16, 16, 8), "123.sv")
 }
