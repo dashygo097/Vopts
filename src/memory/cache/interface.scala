fa3af4bf8a6b004df0db732fd921727ee4de1e62
dashygo097 <dashygo097@gmail.com>
2025-11-03 12:37:18 +0800

    fix: direct_mapped_cache

--- src/memory/cache/interface.scala
+++ src/memory/cache/interface.scala
@@ -2,24 +2,24 @@ package mem.cache
 
 import chisel3._
 
-class CacheEntry(dataWidth: Int, tagWidth: Int) extends Bundle {
+class CacheEntry(dataWidth: Int, tagWidth: Int, wordsPerLine: Int) extends Bundle {
   val tag   = UInt(tagWidth.W)
   val valid = Bool()
-  val data  = UInt(dataWidth.W)
+  val data  = Vec(wordsPerLine, UInt(dataWidth.W))
 }
 
-class CacheIO(addrWidth: Int, dataWidth: Int) extends Bundle {
+class CacheIO(addrWidth: Int, dataWidth: Int, wordsPerLine: Int) extends Bundle {
   val addr       = Input(UInt(addrWidth.W))
-  val write_data = Input(UInt(dataWidth.W))
+  val write_data = Input(UInt((dataWidth * wordsPerLine).W))
   val write_en   = Input(Bool())
   val read_data  = Output(UInt(dataWidth.W))
   val read_en    = Input(Bool())
   val miss       = Output(Bool())
 }
 
-class CacheExternalIO(addrWidth: Int, dataWidth: Int) extends Bundle {
+class CacheExternalIO(addrWidth: Int, dataWidth: Int, wordsPerLine: Int) extends Bundle {
   val ADDR       = Input(UInt(addrWidth.W))
-  val WRITE_DATA = Input(UInt(dataWidth.W))
+  val WRITE_DATA = Input(UInt((dataWidth * wordsPerLine).W))
   val WRITE_EN   = Input(Bool())
   val READ_DATA  = Output(UInt(dataWidth.W))
   val READ_EN    = Input(Bool())
