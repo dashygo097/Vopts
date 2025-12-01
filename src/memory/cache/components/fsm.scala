0e00e0536892f2e6dcb0dc087a2b8f5589283c2a
dashygo097 <dashygo097@github.com>
2025-12-01 22:16:17 +0800

    issue: direct_mapped_cache write_back

--- src/memory/cache/components/fsm.scala
+++ src/memory/cache/components/fsm.scala
@@ -3,10 +3,9 @@ package mem.cache
 import chisel3._
 
 object CacheFSMState extends ChiselEnum {
-  val IDLE = Value(0.U(3.W))
+  val IDLE        = Value(0.U(3.W))
   val COMPARE_TAG = Value(1.U(3.W))
-  val ALLOCATE = Value(2.U(3.W))
-  val WRITEBACK = Value(3.U(3.W))
-  val FLUSH = Value(4.U(3.W))
+  val ALLOCATE    = Value(2.U(3.W))
+  val WRITEBACK   = Value(3.U(3.W))
+  val FLUSH       = Value(4.U(3.W))
 }
-
