a20798753225866575d2fbd86d6f696e163a7e8f
dashygo097 <dashygo097@gmail.com>
2025-12-09 21:52:02 +0800

    rename dual_port_regfile's io name

--- src/memory/register/regfile.scala
+++ src/memory/register/regfile.scala
@@ -4,24 +4,18 @@ import utils._
 import chisel3._
 import chisel3.util._
 
-class DualPortRegFileIO(numRegs: Int, dataWidth: Int) extends Bundle {
-  val rs1_addr   = Input(UInt(log2Ceil(numRegs).W))
-  val rs2_addr   = Input(UInt(log2Ceil(numRegs).W))
-  val write_addr = Input(UInt(log2Ceil(numRegs).W))
-  val write_data = Input(UInt(dataWidth.W))
-  val write_en   = Input(Bool())
-
-  val rs1_data = Output(UInt(dataWidth.W))
-  val rs2_data = Output(UInt(dataWidth.W))
-
-  val rs1_fwd = Output(Bool())
-  val rs2_fwd = Output(Bool())
-}
-
 class DualPortRegFile(numRegs: Int, dataWidth: Int, extraInfo: Seq[Register] = Seq()) extends Module {
   override def desiredName: String = s"dual_port_regfile_b${dataWidth}_r$numRegs"
 
-  val io = IO(new DualPortRegFileIO(numRegs, dataWidth)).suggestName("REGFILE")
+  val rs1_addr   = IO(Input(UInt(log2Ceil(numRegs).W))).suggestName("RS1_ADDR")
+  val rs2_addr   = IO(Input(UInt(log2Ceil(numRegs).W))).suggestName("RS2_ADDR")
+  val write_addr = IO(Input(UInt(log2Ceil(numRegs).W))).suggestName("WRITE_ADDR")
+  val write_data = IO(Input(UInt(dataWidth.W))).suggestName("WRITE_DATA")
+  val write_en   = IO(Input(Bool())).suggestName("WRITE_EN")
+  val rs1_data   = IO(Output(UInt(dataWidth.W))).suggestName("RS1_DATA")
+  val rs2_data   = IO(Output(UInt(dataWidth.W))).suggestName("RS2_DATA")
+  val rs1_fwd    = IO(Output(Bool())).suggestName("RS1_FWD")
+  val rs2_fwd    = IO(Output(Bool())).suggestName("RS2_FWD")
 
   // Memory
   val regs = RegInit(VecInit(Seq.tabulate(numRegs) { addr =>
@@ -43,32 +37,32 @@ class DualPortRegFile(numRegs: Int, dataWidth: Int, extraInfo: Seq[Register] = S
   })
 
   // Write logic
-  when(io.write_en && writableVec(io.write_addr)) {
-    regs(io.write_addr) := io.write_data
+  when(write_en && writableVec(write_addr)) {
+    regs(write_addr) := write_data
   }
 
   // Combinational read with forwarding
-  val rs1_raw = regs(io.rs1_addr)
-  val rs2_raw = regs(io.rs2_addr)
+  val rs1_raw = regs(rs1_addr)
+  val rs2_raw = regs(rs2_addr)
 
   // Forwarding detection
-  val rs1_fwd_match = io.write_en && writableVec(io.write_addr) && (io.rs1_addr === io.write_addr)
-  val rs2_fwd_match = io.write_en && writableVec(io.write_addr) && (io.rs2_addr === io.write_addr)
+  val rs1_fwd_match = write_en && writableVec(write_addr) && (rs1_addr === write_addr)
+  val rs2_fwd_match = write_en && writableVec(write_addr) && (rs2_addr === write_addr)
 
-  io.rs1_fwd := rs1_fwd_match
-  io.rs2_fwd := rs2_fwd_match
+  rs1_fwd := rs1_fwd_match
+  rs2_fwd := rs2_fwd_match
 
   // Read data with forwarding and readable check
-  io.rs1_data := Mux(
+  rs1_data := Mux(
     rs1_fwd_match,
-    io.write_data,
-    Mux(readableVec(io.rs1_addr), rs1_raw, 0.U)
+    write_data,
+    Mux(readableVec(rs1_addr), rs1_raw, 0.U)
   )
 
-  io.rs2_data := Mux(
+  rs2_data := Mux(
     rs2_fwd_match,
-    io.write_data,
-    Mux(readableVec(io.rs2_addr), rs2_raw, 0.U)
+    write_data,
+    Mux(readableVec(rs2_addr), rs2_raw, 0.U)
   )
 }
 
