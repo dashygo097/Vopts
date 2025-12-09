faf0ef45b8657d3de9c9398834c8df7ef949d3dc
dashygo097 <dashygo097@gmail.com>
2025-12-09 23:24:20 +0800

    feat: dual_port_regfile works fine in test cpu

--- src/memory/register/regfile.scala
+++ src/memory/register/regfile.scala
@@ -14,8 +14,6 @@ class DualPortRegFile(numRegs: Int, dataWidth: Int, extraInfo: Seq[Register] = S
   val write_en   = IO(Input(Bool())).suggestName("WRITE_EN")
   val rs1_data   = IO(Output(UInt(dataWidth.W))).suggestName("RS1_DATA")
   val rs2_data   = IO(Output(UInt(dataWidth.W))).suggestName("RS2_DATA")
-  val rs1_fwd    = IO(Output(Bool())).suggestName("RS1_FWD")
-  val rs2_fwd    = IO(Output(Bool())).suggestName("RS2_FWD")
 
   // Memory
   val regs = RegInit(VecInit(Seq.tabulate(numRegs) { addr =>
@@ -41,29 +39,11 @@ class DualPortRegFile(numRegs: Int, dataWidth: Int, extraInfo: Seq[Register] = S
     regs(write_addr) := write_data
   }
 
-  // Combinational read with forwarding
   val rs1_raw = regs(rs1_addr)
   val rs2_raw = regs(rs2_addr)
 
-  // Forwarding detection
-  val rs1_fwd_match = write_en && writableVec(write_addr) && (rs1_addr === write_addr)
-  val rs2_fwd_match = write_en && writableVec(write_addr) && (rs2_addr === write_addr)
-
-  rs1_fwd := rs1_fwd_match
-  rs2_fwd := rs2_fwd_match
-
-  // Read data with forwarding and readable check
-  rs1_data := Mux(
-    rs1_fwd_match,
-    write_data,
-    Mux(readableVec(rs1_addr), rs1_raw, 0.U)
-  )
-
-  rs2_data := Mux(
-    rs2_fwd_match,
-    write_data,
-    Mux(readableVec(rs2_addr), rs2_raw, 0.U)
-  )
+  rs1_data := Mux(readableVec(rs1_addr), rs1_raw, 0.U)
+  rs2_data := Mux(readableVec(rs2_addr), rs2_raw, 0.U)
 }
 
 object TestDualPortRegFile extends App {
