732d72b3d28d4a96d495500ff2a27119953c251c
dashygo097 <dashygo097@github.com>
2026-01-16 01:16:36 +0800

    [math]: wallace tree multiplier for unsigned int

--- src/math/mul/PartialProductGen.scala
+++ src/math/mul/PartialProductGen.scala
@@ -6,38 +6,43 @@ import chisel3.util._
 class PartialProductGen(dw: Int) extends Module {
   override def desiredName: String = s"partial_product_gen_$dw"
 
+  val totalWidth = 2 * dw
+
   val multiplicand    = IO(Input(UInt(dw.W)))
   val multiplier      = IO(Input(UInt(dw.W)))
-  val partialProducts = IO(Output(Vec((dw + 1) / 2, UInt((dw + 3).W))))
+  // Output width is 2 * dw to accommodate full alignment
+  val partialProducts = IO(Output(Vec((dw + 1) / 2, UInt(totalWidth.W))))
 
-  val x_ext = Cat(multiplicand(dw - 1), multiplicand).asSInt
-  val x_2   = (x_ext << 1).asUInt
-
-  val y_ext = Cat(multiplier, 0.U(1.W))
-
-  val encoders = Seq.fill((dw + 1) / 2)(Module(new BoothRadix4))
+  val multiplier_ext = Cat(0.U(2.W), multiplier, 0.U(1.W))
+  val boothEncoders  = Seq.fill((dw + 1) / 2)(Module(new BoothRadix4()))
 
   for (i <- 0 until (dw + 1) / 2) {
-    val y_bits = y_ext(2 * i + 2, 2 * i)
-    encoders(i).y := y_bits
+    val boothBits = if (i == 0) multiplier_ext(2, 0) else multiplier_ext(i * 2 + 2, i * 2)
 
-    val pp_raw = Wire(UInt((dw + 1).W))
-    pp_raw := MuxCase(
-      0.U,
-      Seq(
-        encoders(i).one -> x_ext.asUInt,
-        encoders(i).two -> x_2
-      )
-    )
+    boothEncoders(i).y := boothBits
 
-    val pp_final = Mux(encoders(i).neg, (~pp_raw).asUInt + 1.U, pp_raw)
+    val rawWidth = dw + 2
+    val ppRaw    = Wire(UInt(rawWidth.W))
 
-    val pp_shifted = Cat(
-      Fill(2, pp_final(dw)),
-      pp_final,
-      0.U((2 * i).W)
-    )
+    // Generate Partial Product based on Booth Encodings
+    when(boothEncoders(i).zero) {
+      ppRaw := 0.U
+    }.elsewhen(boothEncoders(i).one && !boothEncoders(i).two) {
+      // +/- 1 * M
+      ppRaw := Mux(boothEncoders(i).neg, (~Cat(0.U(2.W), multiplicand)).asUInt + 1.U, Cat(0.U(2.W), multiplicand))
+    }.elsewhen(boothEncoders(i).two && !boothEncoders(i).one) {
+      // +/- 2 * M (Shift left by 1)
+      ppRaw := Mux(boothEncoders(i).neg, (~Cat(0.U(1.W), multiplicand, 0.U(1.W))).asUInt + 1.U, Cat(0.U(1.W), multiplicand, 0.U(1.W)))
+    }.otherwise {
+      ppRaw := 0.U
+    }
 
-    partialProducts(i) := pp_shifted
+    // Sign Extension Logic
+    val signBit        = ppRaw(rawWidth - 1)
+    val ppSignExtended = Wire(UInt(totalWidth.W))
+    val fillLen        = (totalWidth - rawWidth).max(0)
+    ppSignExtended := Cat(Fill(fillLen, signBit), ppRaw)
+
+    partialProducts(i) := ppSignExtended << (2 * i).U
   }
 }
