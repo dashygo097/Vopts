07cdb24cd38a719b4cd1effe7b3819813452ee10
dashygo097 <dashygo097@github.com>
2026-01-20 11:33:44 +0800

    [mem.cache] rename for cache

--- src/memory/cache/variants/DirectMapped.scala
+++ src/memory/cache/variants/DirectMapped.scala
@@ -9,14 +9,12 @@ class DirectMappedCache(
   dataWidth: Int,
   wordsPerLine: Int,
   numLines: Int,
-  pipe: Boolean = false
 ) extends Module {
   override def desiredName: String = s"direct_mapped_cache_${addrWidth}x${dataWidth}x${wordsPerLine}x$numLines"
 
-  val ext_io = IO(new CacheExternalIO(addrWidth, dataWidth, wordsPerLine)).suggestName("CACHE")
-  val io     = Wire(new CacheIO(addrWidth, dataWidth, wordsPerLine))
-
-  dontTouch(ext_io)
+  val upper = IO(Flipped(new UnifiedMemoryIO(addrWidth, dataWidth, 1, wordsPerLine)))
+  val lower = IO(new UnifiedMemoryIO(addrWidth, dataWidth, wordsPerLine, wordsPerLine))
+  val miss  = IO(Output(Bool()))
 
   // Parameters
   val tagWidth        = addrWidth - log2Ceil(numLines) - log2Ceil(wordsPerLine) - log2Ceil(dataWidth / 8)
@@ -81,27 +79,27 @@ class DirectMappedCache(
   val hit  = meta.alloc && (meta.tag === reqTag)
 
   // Default outputs
-  io.upper.req.ready      := false.B
-  io.upper.resp.valid     := false.B
-  io.upper.resp.bits.data := 0.U
-  io.lower.req.valid      := false.B
-  io.lower.req.bits.addr  := 0.U
-  io.lower.req.bits.data  := 0.U
-  io.lower.req.bits.op    := MemoryOp.READ
-  io.lower.resp.ready     := false.B
-  io.miss                 := false.B
+  upper.req.ready      := false.B
+  upper.resp.valid     := false.B
+  upper.resp.bits.data := 0.U
+  lower.req.valid      := false.B
+  lower.req.bits.addr  := 0.U
+  lower.req.bits.data  := 0.U
+  lower.req.bits.op    := MemoryOp.READ
+  lower.resp.ready     := false.B
+  miss                 := false.B
 
   // FSM
   switch(state) {
     is(CacheFSMState.IDLE) {
-      io.upper.req.ready := true.B
-      memReqSent         := false.B
+      upper.req.ready := true.B
+      memReqSent      := false.B
 
-      when(io.upper.req.fire) {
-        val parsed = parseAddr(io.upper.req.bits.addr)
-        reqAddr       := io.upper.req.bits.addr
-        reqData       := io.upper.req.bits.data
-        reqOp         := io.upper.req.bits.op
+      when(upper.req.fire) {
+        val parsed = parseAddr(upper.req.bits.addr)
+        reqAddr       := upper.req.bits.addr
+        reqData       := upper.req.bits.data
+        reqOp         := upper.req.bits.op
         reqTag        := parsed.tag
         reqIndex      := parsed.index
         reqWordOffset := parsed.wordOffset
@@ -119,13 +117,13 @@ class DirectMappedCache(
     is(CacheFSMState.COMPARE_TAG) {
       when(hit) {
         // Cache hit
-        io.miss := false.B
+        miss := false.B
         when(reqOp === MemoryOp.READ) {
           // Read hit - return the specific word
-          io.upper.resp.valid     := true.B
-          io.upper.resp.bits.data := extractWord(currentLineData, reqWordOffset)
+          upper.resp.valid     := true.B
+          upper.resp.bits.data := extractWord(currentLineData, reqWordOffset)
 
-          when(io.upper.resp.ready) {
+          when(upper.resp.ready) {
             state := CacheFSMState.IDLE
           }
         }.otherwise {
@@ -140,14 +138,14 @@ class DirectMappedCache(
           lastWriteIndex := reqIndex
           lastWriteData  := updatedLine
 
-          io.upper.resp.valid := true.B
-          when(io.upper.resp.ready) {
+          upper.resp.valid := true.B
+          when(upper.resp.ready) {
             state := CacheFSMState.IDLE
           }
         }
       }.otherwise {
         // Cache miss
-        io.miss := true.B
+        miss := true.B
         when(lastWriteValid && (lastWriteIndex === reqIndex)) {
           lastWriteValid := false.B
         }
@@ -167,12 +165,12 @@ class DirectMappedCache(
 
     is(CacheFSMState.WRITEBACK) {
       // Write back dirty line to memory
-      io.lower.req.valid     := true.B
-      io.lower.req.bits.op   := MemoryOp.WRITE
-      io.lower.req.bits.addr := Cat(meta.tag, reqIndex, 0.U((wordOffsetWidth + byteOffsetWidth).W))
-      io.lower.req.bits.data := currentLineData
+      lower.req.valid     := true.B
+      lower.req.bits.op   := MemoryOp.WRITE
+      lower.req.bits.addr := Cat(meta.tag, reqIndex, 0.U((wordOffsetWidth + byteOffsetWidth).W))
+      lower.req.bits.data := currentLineData
 
-      when(io.lower.req.ready) {
+      when(lower.req.ready) {
         metaArray(reqIndex).dirty := false.B
         state                     := CacheFSMState.ALLOCATE
       }
@@ -182,28 +180,28 @@ class DirectMappedCache(
       val currParsed = parseAddr(reqAddr)
 
       when(!memReqSent) {
-        io.lower.req.valid     := true.B
-        io.lower.req.bits.op   := MemoryOp.READ
-        io.lower.req.bits.addr := Cat(currParsed.tag, currParsed.index, 0.U((wordOffsetWidth + byteOffsetWidth).W))
-        io.lower.req.bits.data := 0.U
+        lower.req.valid     := true.B
+        lower.req.bits.op   := MemoryOp.READ
+        lower.req.bits.addr := Cat(currParsed.tag, currParsed.index, 0.U((wordOffsetWidth + byteOffsetWidth).W))
+        lower.req.bits.data := 0.U
 
-        when(io.lower.req.ready) {
+        when(lower.req.ready) {
           memReqSent := true.B
         }
       }
 
       // Always assert ready to accept response
-      io.lower.resp.ready := true.B
+      lower.resp.ready := true.B
 
-      when(memReqSent && io.lower.resp.fire) {
+      when(memReqSent && lower.resp.fire) {
         val newLineData = Wire(UInt(lineWidth.W))
 
         when(reqOp === MemoryOp.WRITE) {
           // Write allocate - update the word being written
-          newLineData               := updateWord(io.lower.resp.bits.data, reqWordOffset, reqData)
+          newLineData               := updateWord(lower.resp.bits.data, reqWordOffset, reqData)
           metaArray(reqIndex).dirty := true.B
         }.otherwise {
-          newLineData               := io.lower.resp.bits.data
+          newLineData               := lower.resp.bits.data
           metaArray(reqIndex).dirty := false.B
         }
 
@@ -221,24 +219,20 @@ class DirectMappedCache(
         lastWriteData  := newLineData
 
         // Return data to CPU
-        io.upper.resp.valid     := true.B
-        io.upper.resp.bits.data := Mux(
+        upper.resp.valid     := true.B
+        upper.resp.bits.data := Mux(
           reqOp === MemoryOp.READ,
           extractWord(newLineData, reqWordOffset),
           reqData
         )
 
-        when(io.upper.resp.ready) {
+        when(upper.resp.ready) {
           state      := CacheFSMState.IDLE
           memReqSent := false.B
         }
       }
     }
   }
-
-  if (pipe) ext_io.connectRegNext(io)
-  else
-    ext_io.connect(io)
 }
 
 object TestDirectMappedCache extends App {
