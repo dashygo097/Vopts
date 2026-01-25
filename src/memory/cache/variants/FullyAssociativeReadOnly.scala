3af73639a66b71b649e4c88eb711bcd8315140f0
dashygo097 <dashygo097@github.com>
2026-01-25 17:34:24 +0800

    [com.cache] support little and big endien

--- src/memory/cache/variants/FullyAssociativeReadOnly.scala
+++ src/memory/cache/variants/FullyAssociativeReadOnly.scala
@@ -9,6 +9,7 @@ class FullyAssociativeCacheReadOnly(
   wordsPerLine: Int,
   numLines: Int,
   replPolicy: ReplacementPolicy,
+  bigEndian: Boolean = true
 ) extends Module {
   override def desiredName: String = s"fully_associative_cache_read_only_${addrWidth}x${dataWidth}x${wordsPerLine}x$numLines"
 
@@ -49,21 +50,32 @@ class FullyAssociativeCacheReadOnly(
     val byteOffset = addr(byteOffsetWidth - 1, 0)
   }
 
-  // Extract word from cache line (BIG ENDIAN - MSB is word 0)
-  def extractWord(lineData: UInt, wordOffset: UInt): UInt = {
-    val words = VecInit((0 until wordsPerLine).reverse.map(i => lineData((i + 1) * dataWidth - 1, i * dataWidth)))
-    words(wordOffset)
-  }
+  // Extract word from cache line
+  def extractWord(lineData: UInt, wordOffset: UInt): UInt =
+    if (bigEndian) {
+      val words = VecInit((0 until wordsPerLine).reverse.map(i => lineData((i + 1) * dataWidth - 1, i * dataWidth)))
+      words(wordOffset)
+    } else {
+      val words = VecInit((0 until wordsPerLine).map(i => lineData((i + 1) * dataWidth - 1, i * dataWidth)))
+      words(wordOffset)
+    }
 
-  // Update word in cache line (BIG ENDIAN)
-  def updateWord(lineData: UInt, wordOffset: UInt, newWord: UInt): UInt = {
-    val words = VecInit((0 until wordsPerLine).map { i =>
-      val currentWord    = lineData((i + 1) * dataWidth - 1, i * dataWidth)
-      val bigEndianIndex = (wordsPerLine - 1 - i).U
-      Mux(wordOffset === bigEndianIndex, newWord, currentWord)
-    })
-    words.asUInt
-  }
+  // Update word in cache line
+  def updateWord(lineData: UInt, wordOffset: UInt, newWord: UInt): UInt =
+    if (bigEndian) {
+      val words = VecInit((0 until wordsPerLine).map { i =>
+        val currentWord    = lineData((i + 1) * dataWidth - 1, i * dataWidth)
+        val bigEndianIndex = (wordsPerLine - 1 - i).U
+        Mux(wordOffset === bigEndianIndex, newWord, currentWord)
+      })
+      words.asUInt
+    } else {
+      val words = VecInit((0 until wordsPerLine).map { i =>
+        val currentWord = lineData((i + 1) * dataWidth - 1, i * dataWidth)
+        Mux(wordOffset === i.U, newWord, currentWord)
+      })
+      words.asUInt
+    }
 
   // Default outputs
   upper.req.ready      := false.B
