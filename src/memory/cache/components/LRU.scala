2df4de83763054e920a8d2dfdb45a25d7f74331c
dashygo097 <dashygo097@gmail.com>
2026-03-10 00:10:34 +0800

    [mem.cache] optimized LRU

--- src/memory/cache/components/LRU.scala
+++ src/memory/cache/components/LRU.scala
@@ -1,5 +1,6 @@
 package vopts.mem.cache
 
+import vopts.utils.CombTree
 import chisel3._
 import chisel3.util._
 
@@ -7,12 +8,24 @@ class LRUState(numLines: Int) extends ReplacementPolicyState(numLines) {
   val ageWidth = log2Ceil(numLines + 1)
   val age      = RegInit(VecInit(Seq.tabulate(numLines)(i => i.U(ageWidth.W))))
 
-  override def getVictim(): UInt =
-    PriorityEncoder(age.map(_ === age.reduce((a, b) => Mux(a > b, a, b))))
+  override def getVictim(): UInt = {
+    val indexBits         = log2Ceil(numLines)
+    val tagged: Seq[UInt] = (0 until numLines).map { i =>
+      Cat(age(i), i.U(indexBits.W))
+    }
+    val maxTagged         = CombTree.treeReduce(tagged) { (a, b) =>
+      Mux(
+        a(indexBits + ageWidth - 1, indexBits) >
+          b(indexBits + ageWidth - 1, indexBits),
+        a,
+        b
+      )
+    }
+    maxTagged(indexBits - 1, 0)
+  }
 
   override def update(accessedIndex: UInt, isHit: Bool): Unit = {
     val currentAge = age(accessedIndex)
-
     for (i <- 0 until numLines)
       when(i.U === accessedIndex) {
         age(i) := 0.U
