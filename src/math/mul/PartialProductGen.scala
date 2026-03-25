0c3f80abfe7dc4c4c9197833e83ddc179c94d318
dashygo097 <dashygo097@github.com>
2026-03-25 12:47:41 +0800

    Reapply "Implemented a high-performance multiplier using Booth-4 encoding and Wallace-tree reduction."
    This reverts commit abbb4e805e7f75d8ec55227d05fabd3ec2643caa.

--- src/math/mul/PartialProductGen.scala
+++ src/math/mul/PartialProductGen.scala
@@ -6,43 +6,50 @@ import chisel3.util._
 class PartialProductGen(dw: Int) extends Module {
   override def desiredName: String = s"partial_product_gen_$dw"
 
-  val totalWidth = 2 * dw
+  val total_width = 2 * dw
 
-  val multiplicand    = IO(Input(UInt(dw.W)))
-  val multiplier      = IO(Input(UInt(dw.W)))
-  // Output width is 2 * dw to accommodate full alignment
-  val partialProducts = IO(Output(Vec((dw + 1) / 2, UInt(totalWidth.W))))
+  val multiplicand        = IO(Input(UInt(dw.W)))
+  val multiplier          = IO(Input(UInt(dw.W)))
+  val multiplicand_signed  = IO(Input(Bool()))
+  val multiplier_signed    = IO(Input(Bool()))
+  // Output width is 2 * dw for full alignment
+  val partial_products = IO(Output(Vec((dw + 1) / 2, UInt(total_width.W))))
 
-  val multiplier_ext = Cat(0.U(2.W), multiplier, 0.U(1.W))
-  val boothEncoders  = Seq.fill((dw + 1) / 2)(Module(new BoothRadix4()))
+  val multiplier_sign = Mux(multiplier_signed, multiplier(dw - 1), 0.U(1.W))
+  val multiplier_ext  = Cat(Fill(2, multiplier_sign), multiplier, 0.U(1.W))
+  val booth_encoders  = Seq.fill((dw + 1) / 2)(Module(new BoothRadix4()))
+
+  val multiplicand_sign      = Mux(multiplicand_signed, multiplicand(dw - 1), 0.U(1.W))
+  val multiplicand_ext       = Cat(Fill(2, multiplicand_sign), multiplicand)
+  val multiplicand_ext_shift1 = Cat(Fill(1, multiplicand_sign), multiplicand, 0.U(1.W))
 
   for (i <- 0 until (dw + 1) / 2) {
-    val boothBits = if (i == 0) multiplier_ext(2, 0) else multiplier_ext(i * 2 + 2, i * 2)
+    val booth_bits = if (i == 0) multiplier_ext(2, 0) else multiplier_ext(i * 2 + 2, i * 2)
 
-    boothEncoders(i).y := boothBits
+    booth_encoders(i).y := booth_bits
 
-    val rawWidth = dw + 2
-    val ppRaw    = Wire(UInt(rawWidth.W))
+    val raw_width = dw + 2
+    val pp_raw   = Wire(UInt(raw_width.W))
 
-    // Generate Partial Product based on Booth Encodings
-    when(boothEncoders(i).zero) {
-      ppRaw := 0.U
-    }.elsewhen(boothEncoders(i).one && !boothEncoders(i).two) {
+    // Generate partial product from Booth encoding
+    when(booth_encoders(i).zero) {
+      pp_raw := 0.U
+    }.elsewhen(booth_encoders(i).one && !booth_encoders(i).two) {
       // +/- 1 * M
-      ppRaw := Mux(boothEncoders(i).neg, (~Cat(0.U(2.W), multiplicand)).asUInt + 1.U, Cat(0.U(2.W), multiplicand))
-    }.elsewhen(boothEncoders(i).two && !boothEncoders(i).one) {
-      // +/- 2 * M (Shift left by 1)
-      ppRaw := Mux(boothEncoders(i).neg, (~Cat(0.U(1.W), multiplicand, 0.U(1.W))).asUInt + 1.U, Cat(0.U(1.W), multiplicand, 0.U(1.W)))
+      pp_raw := Mux(booth_encoders(i).neg, (~multiplicand_ext).asUInt + 1.U, multiplicand_ext)
+    }.elsewhen(booth_encoders(i).two && !booth_encoders(i).one) {
+      // +/- 2 * M (shift left by 1)
+      pp_raw := Mux(booth_encoders(i).neg, (~multiplicand_ext_shift1).asUInt + 1.U, multiplicand_ext_shift1)
     }.otherwise {
-      ppRaw := 0.U
+      pp_raw := 0.U
     }
 
-    // Sign Extension Logic
-    val signBit        = ppRaw(rawWidth - 1)
-    val ppSignExtended = Wire(UInt(totalWidth.W))
-    val fillLen        = (totalWidth - rawWidth).max(0)
-    ppSignExtended := Cat(Fill(fillLen, signBit), ppRaw)
+    // Sign extension to full width
+    val sign_bit        = pp_raw(raw_width - 1)
+    val pp_sign_extended = Wire(UInt(total_width.W))
+    val fill_len        = (total_width - raw_width).max(0)
+    pp_sign_extended := Cat(Fill(fill_len, sign_bit), pp_raw)
 
-    partialProducts(i) := ppSignExtended << (2 * i).U
+    partial_products(i) := pp_sign_extended << (2 * i).U
   }
 }
