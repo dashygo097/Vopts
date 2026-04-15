18c50807d3bf52e01b5667230171020703790f99
dashygo097 <dashygo097@gmail.com>
2026-04-15 12:21:12 +0800

    [mem.cache] reimpled for non-blocking cache

---
+++ src/memory/cache/FSM.scala
@@ -0,0 +1,10 @@
+package vopts.mem.cache
+
+import chisel3._
+
+object CacheFSMState extends ChiselEnum {
+  val IDLE        = Value(0.U(2.W))
+  val COMPARE_TAG = Value(1.U(2.W))
+  val WAIT_WORD   = Value(2.U(2.W))
+  val WAIT_MSHR   = Value(3.U(2.W))
+}
